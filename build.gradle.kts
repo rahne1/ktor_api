2605a1e5cc0e6d8e3fcffa23eef0baf5ba74a7ae
rahne1 <rahnelazevedo@gmail.com>
2024-09-23 11:02:31 +0100

    first commit, progress on implementing multipart functionality in /content/moderate, updated imports and build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,18 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.10")
-    implementation("io.ktor:ktor-server-netty:2.3.10")
-    implementation("ch.qos.logback:logback-classic:1.4.12")
+    implementation("io.ktor:ktor-server-core:2.3.12")
+    implementation("io.ktor:ktor-server-netty:2.3.12")
+    implementation("io.ktor:ktor-server-host-common:2.3.12")
+    implementation("io.ktor:ktor-server-content-negotation:2.3.12"):
+    implementation("io.ktor:ktor-server-status-pages:2.3.12")
+    implementation("io.ktor:ktor-serialization-jackson:2.3.12")
+}
+
+implementation("ch.qos.logback:log-back-classic:1.4.12")
+testImplementation("io.ktor:ktor-server-tests:2.3.12")
+testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.10")
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
--- src/main/kotlin/org/example/Application.kt
+++ src/main/kotlin/org/example/Application.kt
@@ -1,22 +1,77 @@
 package org.example
+import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
 import io.ktor.http.*
-import io.ktor.server.application.*
+import io.ktor.jackson.jackson
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.utils.io.streamas.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.util.*
+
+data class ModerationResponse(
+    val isNegative: Boolean, 
+    val ToxicityScore: Double?, 
+    val sentiment: String,
+)
+data class FileClass(
+    val fileName: UUID,
+    val fileSize: Double,
+)
+
 
 fun main() {
-    embeddedServer(Netty, port = 8080)  {
-        configureRouting()
-    }.start(wait = true)
+    embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation)
+        {
+            jackson {}
+        }
+        install(StatusPages) {
+            exception<Throwable>
+            { cause ->
+            call.respond(HttpStatusCode.InternalServerError, mapOf("error" to cause.localizedMessage))}
+            LoggerFactory.getLogger("Application").error("Unhandled exception," cause)
+        }
+    }
 }
 
 fun Application.configureRouting() {
     routing {
-        post("/content/moderate") {
-            call.respondText("Hello from POST /content/moderate", status = HttpStatusCode.OK)
+        post("/content/moderate") { // NOTE(rahne1): need to actually save the fileclass to database so user can  check status in /content/{content_id}/status and stats of all content classified can be returned by the cache (cached data is classified as all data that is not fully procesed and the last 5 minutes worth of processed data)
+            val multipart = call.recieveMultipart()
+            val contentType: String? = null
+            var imageFile: File? = null
+            multipart.forEachPart { part - >
+            when (part) {
+                is PartData.FormItem -> {
+                    fileDescription = part.value
+                }
+                is PartData.FileItem -> {
+                    fileName = part
+                    val fileName = UUID.randomUUID().toString()
+                    val bytes = part.streamProvider().readBytes()
+                    if (bytes > 52428800) {
+                        call.respond(HttpStatusCode.NotAcceptable, "File size too large. Files must be below 50mb.")call.
+                    }
+                    val fileClass = FileClass(fileName, FileSize)
+
+                    File("uploads/$fileName").writeBytes(fileBytes)
+                }
+
+                else -> {}
+            }
+            part.dispose()
         }
+
+        call.respondText("Hello from POST /content/moderate", status = HttpStatusCode.OK)
+    }   
 
         get("/content/{content_id}/status") {
             val contentId = call.parameters["content_id"]
