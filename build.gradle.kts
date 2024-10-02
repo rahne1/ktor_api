028abd24990a1627300ae40f0990e022d83c2e84
rahne1 <rahnelazevedo@gmail.com>
2024-10-02 15:46:35 +0100

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,18 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.0")
-    implementation("io.ktor:ktor-server-netty:2.3.0")
-    implementation("io.ktor:ktor-server-host-common:2.3.0")
-    implementation("io.ktor:ktor-server-status-pages:2.3.0")
-    implementation("io.ktor:ktor-serialization-jackson:2.3.0")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
+    implementation("io.ktor:ktor-server-core:3.0.0-rc-1")
+    implementation("io.ktor:ktor-server-netty:3.0.0-rc-1")
+    implementation("io.ktor:ktor-server-host-common:3.0.0-rc-1")
+    implementation("io.ktor:ktor-server-status-pages:3.0.0-rc-1")
+    implementation("io.ktor:ktor-serialization-jackson:3.0.0-rc-1")
+    implementation("io.ktor:ktor-server-content-negotiation:3.0.0-rc-1")
     implementation("ch.qos.logback:logback-classic:1.5.8")
     implementation("org.jetbrains.exposed:exposed-core:0.55.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.55.0")
-    testImplementation("io.ktor:ktor-server-tests:2.3.0")
+    implementation("org.postgresql:postgresql:42.7.4")
+    testImplementation("io.ktor:ktor-server-tests:3.0.0-rc-1")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.10")
 }
 
