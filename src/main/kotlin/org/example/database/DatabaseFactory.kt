560ac1d7444aeca95f85f0ed002e18d3a914a28c
Rahnel Azevedo <158081602+rahne1@users.noreply.github.com>
2024-10-23 12:48:58 +0100

    Update DatabaseFactory.kt

--- src/main/kotlin/org/example/database/DatabaseFactory.kt
+++ src/main/kotlin/org/example/database/DatabaseFactory.kt
@@ -1,19 +1,29 @@
 package org.example.database
 
 import kotlinx.coroutines.Dispatchers
+import org.example.models.Content
+import org.example.models.ContentStorage
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init() {
         val driverClassName = "org.postgresql.Driver"
-        val jdbcURL = "jdbc:POSTGRESURL"
-        val user = "YOUR USERNAME HERE"
-        val password = "YOUR PASSWORD HERE"
-
+        val jdbcURL = "placeholder"
+        val user = "placeholder"
+        val password = "placeholder"
         Database.connect(jdbcURL, driverClassName, user, password)
+        createTables()
+    }
+
+    private fun createTables() {
+        transaction {
+            SchemaUtils.create(Content, ContentStorage)
+        }
     }
 
     suspend fun <T> dbQuery(block: suspend () -> T): T =
-    newSuspendedTransaction(Dispatchers.IO) { block() }
-}
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+} s
